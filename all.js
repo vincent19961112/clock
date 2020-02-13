d402a5ac3bfa24644dc779878826eeb63cf1dc1b
vincent <v19961112@gmail.com>
2020-02-13 15:08:57 +0800

    add js code

---
+++ all.js
@@ -0,0 +1,22 @@
+function abc() {
+    var time = new Date();
+    var hr = time.getHours();
+    var min = time.getMinutes();
+    var sec = time.getSeconds();
+    alert(hr + "hr" + min + "min" + sec + "sec");
+}
+abc();
+
+(function() {
+    var time = new Date(),
+        second = time.getSeconds() / 60 * 360,
+        minute = time.getMinutes() / 60 * 360 + time.getSeconds() / 60 * 6,
+        hour = time.getHours() / 12 * 360 + time.getMinutes() / 60 * 30,
+
+        animation = [
+            "@Keyframes sec-hand{from{transform:DeviceRotationRate(" + second + "deg);}to{transform:rotate(" + (second + 360) + "deg);)}",
+            "@Keyframes min-hand{from{transform:DeviceRotationRate(" + minute + "deg);}to{transform:rotate(" + (minute + 360) + "deg);)}",
+            "@Keyframes hr-hand{from{transform:DeviceRotationRate(" + hour + "deg);}to{transform:rotate(" + (hour + 360) + "deg);)}",
+        ].join("");
+    document.querySelector("#clock-animate").innerHTML = animation;
+})();
