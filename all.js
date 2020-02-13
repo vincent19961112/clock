4e1278587e33fb80e8d005b1c6d8de86cf117ba5
vincent <v19961112@gmail.com>
2020-02-13 15:59:10 +0800

    fix

--- all.js
+++ all.js
@@ -5,9 +5,9 @@
         hour = time.getHours() / 12 * 360 + time.getMinutes() / 60 * 30,
 
         animation = [
-            "@Keyframes sec-hand{from{transform:DeviceRotationRate(" + second + "deg);}to{transform:rotate(" + (second + 360) + "deg);)}",
-            "@Keyframes min-hand{from{transform:DeviceRotationRate(" + minute + "deg);}to{transform:rotate(" + (minute + 360) + "deg);)}",
-            "@Keyframes hr-hand{from{transform:DeviceRotationRate(" + hour + "deg);}to{transform:rotate(" + (hour + 360) + "deg);)}",
+            "@Keyframes sec-hand{from{transform:rotate(" + second + "deg);}to{transform:rotate(" + (second + 360) + "deg);)}",
+            "@Keyframes min-hand{from{transform:rotate(" + minute + "deg);}to{transform:rotate(" + (minute + 360) + "deg);)}",
+            "@Keyframes hr-hand{from{transform:rotate(" + hour + "deg);}to{transform:rotate(" + (hour + 360) + "deg);)}"
         ].join("");
     document.querySelector("#clock-animate").innerHTML = animation;
 })();
